a9b8a99e870b315337fb13d77c6f82182e59989d
JinKyung Kim <kimradium@naver.com>
2021-12-13 00:21:34 +0900

    #5 Route 생성

--- src/Components/MovieCard/index.js
+++ src/Components/MovieCard/index.js
@@ -14,35 +14,37 @@ const MovieCard = (props) => {
 
   return (
     <div className="py-3 sm:max-w-xl sm:mx-auto my-20">
-      <div className="bg-white shadow-lg border-gray-100 max-h-80	 border rounded-3xl p-8 flex space-x-8">
-        <div className="h-48 overflow-visible w-1/2">
-          <img
-            className="rounded-3xl shadow-lg"
-            src={movies.large_cover_image}
-            alt=""
-          />
-        </div>
-        <div className="flex flex-col w-1/2 space-y-4">
-          <div className="flex justify-between items-start">
-            <h2 className="text-2xl font-bold">{movies.title_english}</h2>
-            <div className="bg-yellow-400 font-bold rounded-xl p-2">
-              {movies.rating}
+      <a href={movies.url} target="_blank" rel="noopener noreferrer">
+        <div className="bg-white hover:shadow-lg shadow-md border-gray-100 max-h-full	md:max-h-80  border rounded-3xl p-8 flex flex-col md:flex-row space-x-8">
+          <div className="h-48 md:overflow-visible md:w-1/2">
+            <img
+              className="rounded-3xl shadow-lg cursor-pointer h-48 md:h-80 mx-auto"
+              src={movies.large_cover_image}
+              alt=""
+            />
+          </div>
+          <div className="flex flex-col md:w-1/2 space-y-4">
+            <div className="flex justify-between items-start">
+              <h2 className="text-2xl font-bold">{movies.title_english}</h2>
+              <div className="bg-yellow-400 font-bold rounded-xl p-2">
+                {movies.rating}
+              </div>
+            </div>
+            <div>
+              <div className="flex-auto flex-row text-sm text-gray-400">
+                {rendering()}
+              </div>
+              <div className="text-lg text-gray-800">{movies.year}</div>
+            </div>
+            <p className=" text-gray-400 max-h-40 overflow-y-hidden">
+              {movies.summary}
+            </p>
+            <div className="flex text-2xl font-bold text-a">
+              {movies.runtime} min
             </div>
           </div>
-          <div>
-            <div className="flex-auto flex-row text-sm text-gray-400">
-              {rendering()}
-            </div>
-            <div className="text-lg text-gray-800">{movies.year}</div>
-          </div>
-          <p className=" text-gray-400 max-h-40 overflow-y-hidden">
-            {movies.summary}
-          </p>
-          <div className="flex text-2xl font-bold text-a">
-            {movies.runtime} min
-          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
---
+++ src/Routes/Home.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { MovieApi } from "../API/MovieApi";
+import MovieCard from "../Components/MovieCard";
+
+const Home = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const GetMovieAPI = async () => {
+    // 요청이 시작 할 때 초기화
+    setError(null);
+    setMovies(null);
+    setLoading(true);
+    const MovieResponse = await MovieApi.Get_Movie("")
+      .then((response) => {
+        console.log("movie", response.data.data.movies);
+        setMovies(response.data.data.movies);
+      })
+      .catch((e) => {
+        setError(e);
+      });
+    setLoading(false);
+    return MovieResponse;
+  };
+
+  useEffect(() => {
+    GetMovieAPI();
+  }, []);
+
+  if (loading) return <div>잠시만 기다려 주세요</div>;
+  if (error)
+    return (
+      <>
+        <div>API 에러가 발생했습니다</div>
+      </>
+    );
+  if (!movies) return null;
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 grid-flow-row gap-3 bg-gray-100 px-6">
+      {movies.map((value) => (
+        <MovieCard key={value.id} data={value} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
