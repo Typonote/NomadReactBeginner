e8902a91b43796a0e9e05bbf08d72945d058a31c
JinKyung Kim <kimradium@naver.com>
2021-12-15 19:14:19 +0900

    #9 todo 완료

--- src/Routes/Todos.js
+++ src/Routes/Todos.js
@@ -2,10 +2,26 @@ import React, { useState } from "react";
 
 const Todos = () => {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
 
   const onChangeHandler = (e) => {
     setToDo(e.target.value);
-    console.log("fff", toDo);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+
+    setToDos((currnetArray) => [toDo, ...currnetArray]);
+    setToDo("");
+  };
+
+  const RemoveTodo = (index) => {
+    const newToDos = [...toDos];
+    newToDos.splice(index, 1);
+    setToDos(newToDos);
   };
 
   return (
@@ -13,9 +29,9 @@ const Todos = () => {
       <div className="bg-white rounded shadow-lg p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
         <div className="mb-4">
           <h1 className="text-gray-500 text-xl font-semibold flex items-center justify-center ">
-            Todo List
+            Todo List ({toDos.length})
           </h1>
-          <form>
+          <form onSubmit={onSubmitHandler}>
             <div className="flex mt-4">
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-black"
@@ -30,15 +46,23 @@ const Todos = () => {
           </form>
         </div>
         <div>
-          <div className="flex mb-4 items-center">
-            <p className="w-full text-black ml-2">방 치우기</p>
-            <button className="flex-no-shrink  p-2 ml-2 border-green-500 border-2 rounded text-green-500  hover:text-white hover:bg-green-500">
-              Done
-            </button>
-            <button className="flex-no-shrink p-2 ml-2 border-red-500 border-2 rounded text-red-500 hover:text-white hover:bg-red-500">
-              Remove
-            </button>
-          </div>
+          {toDos.map((value, index) => (
+            <div className="flex mb-4 items-center" key={index}>
+              <p
+                className="w-full text-black ml-2"
+                style={{ textDecoration: value.isDone ? "line-through" : "" }}
+              >
+                {index + 1}. {value}
+              </p>
+
+              <button
+                className="flex-no-shrink p-2 ml-2 border-red-500 border-2 rounded text-red-500 hover:text-white hover:bg-red-500"
+                onClick={RemoveTodo}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
